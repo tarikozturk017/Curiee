1ce70d78819b1371ee08d965b925902b6c7a82f2
tarikozturk017 <tarik.ozturkk1@gmail.com>
2023-02-26 13:11:52 -0500

    exercise model added

--- api/models/Patient.js
+++ api/models/Patient.js
@@ -1,6 +1,24 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
+const ExerciseSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    diseaseTreatment: [{
+        type: String
+    }],
+    timeStamp: {
+        type: String,
+        default: Date.now()
+    },
+})
+
 const PatientSchema = new Schema({
     firstName: {
         type: String,
@@ -14,12 +32,18 @@ const PatientSchema = new Schema({
         type: String,
         required: true
     },
-    timeStamp: {
+    exercises: [{ 
+        type: Schema.Types.ObjectId, ref: 'Exercise' 
+    }],
+   timeStamp: {
         type: String,
         default: Date.now()
     },
 })
 
-const Patient = mongoose.model("Patient", PatientSchema);
 
-module.exports = Patient;
+const Patient = mongoose.model("Patient", PatientSchema);
+const Exercise = mongoose.model("Exercise", ExerciseSchema);
+
+
+module.exports = {Patient, Exercise};
--- api/server.js
+++ api/server.js
@@ -16,14 +16,19 @@ mongoose.connect(uri, {
     () => console.log("Connected to DB")
     ).catch(console.error)
 
-const Patient = require('./models/Patient');
+// import Patient model
+const Model = require('./models/Patient');
+const Patient = Model.Patient
+const Exercise = Model.Exercise
 
+// get all the patients
 app.get('/patients', async (req, res) => {
     const patients = await Patient.find();
 
     res.json(patients);
 })
 
+// Create new patient
 app.post('/patient/new', (req, res) => {
     console.log(req.body)
     const patient = new Patient({
@@ -36,11 +41,37 @@ app.post('/patient/new', (req, res) => {
     res.json(patient)
 })
 
+// update patient
+app.put('/patient/edit/:id', async (req, res) => {
+    const result = await Patient.findById(req.params.id); // get the patient
 
+    result.exercises.push(req.body.exerciseId);
+    result.save();
+})
+
+// Delete patient by id
 app.delete('/patient/delete/:id', async (req, res) => {
     const result = await Patient.findByIdAndDelete(req.params.id);
 
     res.json(result);
+})
+
+// get all exercises
+app.get('/exercises', async (req, res) => {
+    const exercises = await Exercise.find();
+    res.json(exercises);
+})
+
+// create a new exercise
+app.post('/exercise/new', (req, res) => {
+    console.log(req.body);
+    const exercise = new Exercise({
+        title: req.body.title,
+        description: req.body.description,
+    })
+
+    exercise.save()
+    res.json(exercise)
 })
 
 app.listen(3001, () => console.log("Server started on port 3001"))
