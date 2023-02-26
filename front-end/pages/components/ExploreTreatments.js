a07ffa67cfabd721c0bc02f1a620fc178d97fd0a
tarikozturk017 <tarik.ozturkk1@gmail.com>
2023-02-26 15:20:10 -0500

    explore treatment & creat new treatment - Simple V. created

--- front-end/pages/components/ExploreTreatments.js
+++ front-end/pages/components/ExploreTreatments.js
@@ -1,14 +1,45 @@
-const ExploreTreatment = (props) => {
+import React, { useState, useEffect } from 'react';
 
-    return (
-        <div className=' '>
-            {/* <MyPatients /> */}        
-            <div className=' mx-auto rounded-lg p-5 bg-blue-100 max-w-max text-center'>
-                <h3 className=' text-lg underline'>ExploreTreatment</h3>
-                
-            </div>
-        </div>
-    )
+function ExploreTreatment() {
+  const [exercises, setExercises] = useState([]);
+
+  useEffect(() => {
+    async function fetchExercises() {
+      try {
+        const response = await fetch('http://localhost:3001/exercises');
+        const data = await response.json();
+
+        if (response.ok) {
+          setExercises(data);
+        } else {
+          console.log('Error fetching exercises');
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchExercises();
+  }, []);
+
+  return (
+    <div className=' mx-auto rounded-lg p-5 bg-blue-100 max-w-max text-center'>
+      <h1>Explore Treatment</h1>
+      <ul>
+        {exercises.map((exercise) => (
+          <li key={exercise._id}>
+            <h2>{exercise.title}</h2>
+            <p>{exercise.description}</p>
+            <ul>
+              {exercise.diseaseTreatment.map((treatment, index) => (
+                <li key={index}>{treatment}</li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default ExploreTreatment;
--- front-end/pages/components/NewTreatment.js
+++ front-end/pages/components/NewTreatment.js
@@ -1,14 +1,70 @@
-const NewTreatment = (props) => {
+import React, { useState } from 'react';
 
-    return (
-        <div className=' '>
-            {/* <MyPatients /> */}        
-            <div className=' mx-auto rounded-lg p-5 bg-blue-100 max-w-max text-center'>
-                <h3 className=' text-lg underline'>NewTreatment</h3>
-                
-            </div>
+function NewTreatment() {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [diseaseTreatment, setDiseaseTreatment] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:3001/exercise/new', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          title,
+          description,
+          diseaseTreatment: [diseaseTreatment]
+        })
+      });
+
+      if (response.ok) {
+        console.log('Exercise created successfully');
+      } else {
+        console.log('Error creating exercise');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className=' mx-auto rounded-lg p-5 bg-blue-100 max-w-max text-center'>
+      <h1>Create a new exercise</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            id="title"
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+          />
         </div>
-    )
+        <div>
+          <label htmlFor="description">Description</label>
+          <textarea
+            id="description"
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="diseaseTreatment">Disease Treatment</label>
+          <input
+            type="text"
+            id="diseaseTreatment"
+            value={diseaseTreatment}
+            onChange={(event) => setDiseaseTreatment(event.target.value)}
+          />
+        </div>
+        <button type="submit">Create Exercise</button>
+      </form>
+    </div>
+  );
 }
 
 export default NewTreatment;
