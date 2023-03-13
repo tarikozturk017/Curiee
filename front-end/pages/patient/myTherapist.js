446f567fbc254b7d25b9058a820b2134a1e44df9
tarikozturk017 <tarik.ozturkk1@gmail.com>
2023-03-12 20:43:28 -0400

    on adding patient bugs fixed

--- front-end/pages/patient/myTherapist.js
+++ front-end/pages/patient/myTherapist.js
@@ -1,71 +1,81 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { TiTick } from 'react-icons/ti'
 import { useAtom } from 'jotai';
-import { userAtom } from '@/components/patient/SideBar';
+import { userIdAtom } from '@/components/Layout';
+import useSWR from 'swr';
 
 function MyTherapist() {
-  const [patient, setPatient] = useAtom(userAtom)
+  const [patientId] = useAtom(userIdAtom)
+  const [patient, setPatient] = useState()
 
-  const [patientId, setPatientId] = useState(patient._id)
+  const { data: fetchedPatient } = useSWR(`http://localhost:3001/patient/${patientId}`, async (url) => {
+    const res = await fetch(url)
+    return res.json()
+  })
+
+  if(patient) console.log(`patient: ${patient.firstName}`)
+
+  // Update the `patient` state with the fetched data
+  useEffect(() => {
+    if (fetchedPatient) {
+      setPatient(fetchedPatient)
+    }
+  }, [fetchedPatient])
 
   const handleAccept = async (therapistId) => {
-    // console.log("therapistId: asdasd: "+therapistId)
-
     try {
-        //sendTherapistRequest
-        const response = await fetch(`http://localhost:3001/patient/acceptTherapistRequest`, {
+      //sendTherapistRequest
+      const response = await fetch(`http://localhost:3001/patient/acceptTherapistRequest`, {
         method: 'PUT',
         headers: {
-            'Content-Type': 'application/json'
+          'Content-Type': 'application/json'
         },
         body: JSON.stringify({ patientId, therapistId })
-        
-    });
-    if(response.status === 200) {
-        response.json().then(data => {
-            console.log(data)
-            setPatient(data)
-        })
-    }
+
+      });
+      if (response.status === 200) {
+        const data = await response.json();
+        setPatient(data)
+      }
     } catch (error) {
-    console.error(error);
+      console.error(error);
     }
   }
 
   return (
     <>
-    <div className=' mx-auto rounded-lg p-5 text-2xl bg-blue-100 max-w-max text-center'>
-      
-      {patient.therapists.length > 0 ? (
-        <>
-          <h3>Therapists:</h3>
-          <ul>
-            {patient.therapists.map((therapist) => (
-              <li key={therapist._id}>
-                {therapist.firstName} {therapist.lastName}
-              </li>
-            ))}
-          </ul>
-        </>
-      ) : (
-        <p>You have no therapist.</p>
-      )}
+      {patient && <div className=' mx-auto rounded-lg p-5 text-2xl bg-blue-100 max-w-max text-center'>
 
-      {patient.pendingTherapists.length > 0 ? (
-        <>
-          <h3>Pending Therapists:</h3>
-          <ul>
-            {patient.pendingTherapists.map((therapist) => (
-              <li className=' flex justify-around' key={therapist._id}>
-                {therapist.firstName} {therapist.lastName} <span onClick={ () => handleAccept(therapist._id)}><TiTick /></span>
-              </li>
-            ))}
-          </ul>
-        </>
-      ) : (
-        <p>You have no pending therapist.</p>
-      )}
-      </div>
+        {patient.therapists.length > 0 ? (
+          <>
+            <h3>Therapists:</h3>
+            <ul>
+              {patient.therapists.map((therapist) => (
+                <li key={therapist._id}>
+                  {therapist.firstName} {therapist.lastName}
+                </li>
+              ))}
+            </ul>
+          </>
+        ) : (
+          <p>You have no therapist.</p>
+        )}
+
+        {patient.pendingTherapists.length > 0 ? (
+          <>
+            <h3>Pending Therapists:</h3>
+            <ul>
+              {patient.pendingTherapists.map((therapist) => (
+                <li className=' flex justify-around' key={therapist._id}>
+                  {therapist.firstName} {therapist.lastName} <span onClick={() => handleAccept(therapist._id)}><TiTick /></span>
+                </li>
+              ))}
+            </ul>
+          </>
+        ) : (
+          <p>You have no pending therapist.</p>
+        )}
+      </div>}
     </>
   );
 }
