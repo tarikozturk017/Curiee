f2a6b3bb23300dfbb10b595ff27b5a34abc35f17
tarikozturk017 <tarik.ozturkk1@gmail.com>
2023-07-11 22:09:02 -0400

    explore therapist UI update

---
+++ front-end/components/therapist/TherapistCard.js
@@ -0,0 +1,36 @@
+import Link from "next/link";
+
+const TherapistCard = ({therapist}) => {
+    return (
+        <div className="shadow-lg shadow-blue-300/20 mx-auto rounded-lg py-2 max-w-[40%] mb-5" style={{ backgroundColor: 'rgba(255, 255, 255, 0.11)' }}>
+  <li key={therapist._id}>
+    <Link href={"/therapist/" + therapist._id}>
+      <div className="flex">
+        <div className="my-5 text-center  flex items-center ml-4">
+          <img
+            src="https://mdbcdn.b-cdn.net/img/new/avatars/1.webp"
+            className="w-16 rounded-full shadow-xl shadow-blue-400/40"
+            alt="Avatar"
+          />
+          <div className="text-left ml-3">
+            <h5 className="mt-5 text-xl font-medium leading-tight">
+              {therapist.firstName} {therapist.lastName}
+            </h5>
+            <p className="text-neutral-500 ">{therapist?.occupation} occupation</p>
+          </div>
+        </div>
+        <div className="flex items-center ml-auto mr-4">
+          <p className="">
+            <span className="font-bold">Satisfaction rate:</span>
+            {!therapist?.totalRates ? ' N/A' : therapist.rateCount / therapist.totalRates}{' '}
+            <span>({therapist?.totalRates})</span>
+          </p>
+        </div>
+      </div>
+    </Link>
+  </li>
+</div>
+    )
+}
+
+export default TherapistCard;
--- front-end/pages/therapists.js
+++ front-end/pages/therapists.js
@@ -1,5 +1,9 @@
 import useSWR from 'swr'
 import Link from 'next/link'
+import TherapistCard from '@/components/therapist/TherapistCard'
+import Card from '@/components/page/Card'
+import Header from '@/components/page/Header'
+
 //http://localhost:3001/therapist/all
 
 const Therapists = () => {
@@ -13,18 +17,20 @@ const Therapists = () => {
     if (!therapists) return <div className=' mx-auto rounded-lg p-5 bg-blue-100 max-w-max text-center'>Loading...</div>
 
     return (
-        <>
-        <div className=' mx-auto rounded-lg p-5 text-2xl bg-blue-100 max-w-max text-center'>
-        <h1 className=' underline'>Therapists</h1>
+        <Card>
+        <Header headline={'Explore Therapists'} subtext={'You can explore the therapists.'}/>
+
+        <div className=' text-center'>
         <br />
         <ul>
             {therapists.map((therapist) => (
             // instead of id, one time token can be created to avoid insecurity
-            <li key={therapist._id}><Link href={"/therapist/" + therapist._id}>{therapist.firstName} {therapist.lastName}</Link></li>
+            // <li key={therapist._id}><Link href={"/therapist/" + therapist._id}>{therapist.firstName} {therapist.lastName}</Link></li>
+            <TherapistCard therapist={therapist}/>
             ))}
         </ul>
         </div>
-        </>
+        </Card>
     )
 }
 
