bcc77066615245c7b2d8b749716d447c6653781d
tarikozturk017 <tarik.ozturkk1@gmail.com>
2023-07-23 15:25:09 -0400

    patient left bar update

--- front-end/components/patient/SideBar.js
+++ front-end/components/patient/SideBar.js
@@ -3,6 +3,11 @@ import { userIdAtom } from "../Layout";
 import { atom, useAtom } from 'jotai';
 import useSWR from 'swr';
 
+import { BiBody } from 'react-icons/bi'
+import { GiHealingShield } from 'react-icons/gi'
+import { MdCreateNewFolder } from 'react-icons/md'
+import { FaUserFriends } from 'react-icons/fa'
+
 const userAtom = atom({});
 const PatientSideBar = () => {
     const [patient, setPatient] = useAtom(userAtom)
@@ -17,16 +22,28 @@ const PatientSideBar = () => {
 
     return (
         <>
-        { p && (<div className="  p-10 text-white bg-slate-900 h-screen absolute min-w-min w-1/6 pt-5" >
-            <p className=" m-5 text-center text-xl">{p.firstName + ' ' + p.lastName}</p>
+        { p && 
+        (<div className=" w-1/6 h-screen absolute bg-gradient-to-tr from-slate-200 via-purple-300 to-violet-400" style={{ display: 'inline-block' }}>
+        <div className=" border-r-2 border-blue-100/25 border-double  p-10 text-white h-screen absolute  w-full m-0 pt-5" style={{ backgroundColor: 'rgba(44, 47, 72, 0.46)' }}>
+
+            <p className=" m-5  text-center text-xl">Explore</p>
             <hr />
-            <ul className=" mt-10 flex flex-col gap-5">
+            {/* <ul className=" mt-10 flex flex-col gap-5">
                 <li><Link href="/patient/dashboard">Home</Link></li>
                 <li><Link href="/myTreatment/">My Treatment</Link></li>
                 <li><Link href="/patient/favoriteTreatments">My Favorite Treatments</Link></li>
                 <li><Link href="/treatments">Explore Treatments</Link></li>
                 <li><Link href="/patient/myTherapist">My Therapist</Link></li>
+            </ul> */}
+
+            <ul className=" mt-2 flex flex-col gap-5 ">
+                <li className=" hover:bg-black hover:opacity-70 rounded duration-300 hover:cursor-pointer p-3 flex"><BiBody className=" text-xl mr-2" /><Link href="/patient/dashboard">Home</Link></li>
+                <li className=" hover:bg-black hover:opacity-70 rounded duration-300 hover:cursor-pointer p-3 flex"><GiHealingShield className=" text-xl  mr-2"/><Link href="/myTreatment/">My Treatment</Link></li>
+                <li className=" hover:bg-black hover:opacity-70 rounded duration-300 hover:cursor-pointer p-3 flex"><MdCreateNewFolder className=" text-xl  mr-2"/><Link href="/patient/favoriteTreatments">My Favorite Treatments</Link></li>
+                <li className=" hover:bg-black hover:opacity-70 rounded duration-300 hover:cursor-pointer p-3 flex"><FaUserFriends className=" text-xl  mr-2"/><Link href="/patient/myTherapist">My Therapist</Link></li>
             </ul>
+
+        </div>
         </div>)
         }
         </>
