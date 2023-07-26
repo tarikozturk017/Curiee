531ee1e447970b093de7d0b02e34f88b97167330
tarikozturk017 <tarik.ozturkk1@gmail.com>
2023-07-26 10:00:34 -0400

    fav treatments / patient UI update

--- front-end/pages/patient/favoriteTreatments.js
+++ front-end/pages/patient/favoriteTreatments.js
@@ -3,6 +3,8 @@ import { userAtom } from '@/components/patient/SideBar';
 import { useAtom } from 'jotai';
 import Link from 'next/link';
 import useSWR from 'swr'
+import Card from '@/components/page/Card';
+import Header from '@/components/page/Header';
 
 const FavoriteTreatments = () => {
     const [patient] = useAtom(userAtom);
@@ -16,21 +18,47 @@ const FavoriteTreatments = () => {
     if (error) return <div className=' mx-auto rounded-lg p-5 bg-blue-100 max-w-max text-center'>Failed to load favorite treatments</div>
     if (!favTreatments) return <div className=' mx-auto rounded-lg p-5 bg-blue-100 max-w-max text-center'>Loading...</div>  
 
-    
+    console.log(favTreatments)
     return (
-        <div className=' mx-auto rounded-lg p-5 text-2xl bg-blue-100 max-w-max text-center'>
-        <h2>My Favorite Treatments</h2>
-        <div className=' mx-auto rounded-lg p-5 text-2xl bg-blue-100 max-w-max text-center'>
-        {favTreatments ? <>
-            <ul>
+        <>
+        <Card>
+        <Header headline={`My Favorite Treatments`} subtext={'Find your favorite treatments below.'} />
+            <div className=" shadow-lg shadow-blue-300/20 mx-auto rounded-lg p-1 w-full m-0 pt-5 max-w-[50%] mb-5" style={{ backgroundColor: 'rgba(255, 255, 255, 0.11)' }}>
+                <table className='w-full'>
+                <thead className="" style={{ backgroundColor: 'rgba(0, 0, 0, 0.45)' }}>
+                <tr>
+                    <th  className="px-6 py-3 " >
+                        Treatment
+                    </th>
+                    <th  className="px-6 py-3" >
+                        Rate
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
                 {favTreatments.map((treatment) => (
-                <li key={treatment._id}><Link href={"/treatment/" + treatment._id}>{treatment.title}</Link></li>
+
+                    <tr key={treatment._id} className="">
+                        <th scope="row" className="">
+                        <Link href={"/treatment/" + treatment._id}>{treatment.title}</Link>        </th>
+                    
+                    <td className="px-6 py-1">
+                        {treatment.patientVoteCount / treatment.patientTotalVotes} ({treatment.patientTotalVotes})
+                    </td>
+                    
+                    
+                    </tr>
                 ))}
-            </ul>
+            </tbody>
+            {favTreatments ? <>
+                <ul>
+                </ul>
+            </>
+            : <>You do not have any favorite treatment</>}
+            </table>
+            </div>
+        </Card>
         </>
-        : <>You do not have any favorite treatment</>}
-        </div>
-        </div>
     );
 };
 
