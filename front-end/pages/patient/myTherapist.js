ee0f43afe5dc97fa8ea54ed3f28b798bd8fe2a33
tarikozturk017 <tarik.ozturkk1@gmail.com>
2023-07-26 10:36:12 -0400

    my therapist UI page update

--- front-end/pages/patient/myTherapist.js
+++ front-end/pages/patient/myTherapist.js
@@ -4,6 +4,8 @@ import { useAtom } from 'jotai';
 import Link from 'next/link';
 import { userIdAtom } from '@/components/Layout';
 import useSWR from 'swr';
+import Card from '@/components/page/Card';
+import Header from '@/components/page/Header';
 
 function MyTherapist() {
   const [patientId] = useAtom(userIdAtom)
@@ -62,25 +64,49 @@ function MyTherapist() {
 
   return (
     <>
-      {patient && <div className=' mx-auto rounded-lg p-5 text-2xl bg-blue-100 max-w-max text-center'>
+      <Card>
+        <Header headline={`My Therapists`} subtext={'Find your active therapists below.'} />
 
-        {patient.therapists.length > 0 ? (
-          <>
-            <h3>Therapists:</h3>
-            <ul>
-              {patient.therapists.map((therapist) => (
-                <li key={therapist._id}> <Link href={"/therapist/" + therapist._id}>{therapist.firstName} {therapist.lastName}</Link></li>
-                // <li key={therapist._id}>
-                //   {therapist.firstName} {therapist.lastName}
-                // </li>
-              ))}
-            </ul>
-          </>
-        ) : (
-          <p>You have no therapist.</p>
-        )}
 
-        {patient.pendingTherapists.length > 0 ? (
+
+        {patient?.exercises  && (
+            // <div className=' mx-auto rounded-lg p-5 bg-blue-100 max-w-max text-center'>
+            <div className=" shadow-lg shadow-blue-300/20 mx-auto rounded-lg p-1 w-full m-0 pt-5 max-w-[50%] mb-5" style={{ backgroundColor: 'rgba(255, 255, 255, 0.11)' }}>
+                <table className='w-full'>
+                <thead className="" style={{ backgroundColor: 'rgba(0, 0, 0, 0.45)' }}>
+                <tr>
+                    <th  className="px-6 py-3" >
+                        Therapist Name
+                    </th>
+                    <th  className="px-6 py-3" >
+                        Rate
+                    </th>
+                    
+                </tr>
+            </thead>
+            <tbody>
+            {patient.therapists.map((therapist) => (
+                // <li key={therapist._id}> <Link href={"/therapist/" + therapist._id}>{therapist.firstName} {therapist.lastName}</Link></li>
+            <tr key={therapist._id} className="">
+                <th scope="row" className="">
+                  <Link href={"/therapist/" + therapist._id}>{therapist.firstName} {therapist.lastName}</Link>
+                </th>
+                <td className="px-6 py-1">
+                    {therapist.rateCount / therapist.totalRates} ({therapist.totalRates})   
+                </td>
+                {patient.therapists.length == 0 && 
+                (<th key={therapist._id} className="">
+                    
+                </th>)}
+            </tr>
+            ))}
+            </tbody>    
+                </table>
+            </div>)}
+
+
+      </Card>
+        {/* {patient.pendingTherapists.length > 0 ? (
           <>
             <h3>Pending Therapists:</h3>
             <ul>
@@ -94,9 +120,8 @@ function MyTherapist() {
             </ul>
           </>
         ) : (
-          <p>You have no pending therapist.</p>
-        )}
-      </div>}
+          <p>You have no pending therapist request.</p>
+        )} */}
     </>
   );
 }
