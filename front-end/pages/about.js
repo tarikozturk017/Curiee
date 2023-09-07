dad0ddd497f12c5df150662bed97ce620cee0ca1
tarikozturk017 <tarik.ozturkk1@gmail.com>
2023-09-07 16:03:53 -0400

    about section update

--- front-end/pages/about.js
+++ front-end/pages/about.js
@@ -1,20 +1,66 @@
 const About = () => {
   return (
-    <section className=" ">
-      <h2 className="text-3xl font-semibold  mb-4">About Curie</h2>
-      <p className="text-gray-600 leading-relaxed">
+    <section className=" w-3/6 mt-12 mx-auto text-blue-100">
+      <h2 className="text-3xl font-semibold text-orange-400 mb-4">
+        About Curie
+      </h2>
+      <p className="leading-relaxed ">
         Curie is a revolutionary healthcare platform designed to make healthcare
-        accessible to everyone. Our mission is to provide high-quality medical
-        information and connect patients with the best healthcare providers in
-        the industry.
+        accessible to everyone. Our mission is to help patients to find best
+        therapists and treatments for their diseases. While it helps therapists
+        to promote themselves, it also enable patients connect patients with the
+        best healthcare providers in the industry.
       </p>
-      <p className="text-gray-600 leading-relaxed mt-4">With Curie, you can:</p>
+      <p className="leading-relaxed mt-4 font-bold">With Curie, you can:</p>
       <ul className="list-disc list-inside  ml-6">
-        <li>Access a vast library of medical resources and articles.</li>
-        <li>Find and book appointments with top doctors and specialists.</li>
-        <li>Get personalized health recommendations and tracking.</li>
-        <li>Join a community of health-conscious individuals.</li>
+        <li>
+          Enables to explore treatment models, and also find therapists with
+          high rate
+        </li>
+        <li>
+          Allows therapists/patients connections so that it will enhance the
+          recovery process
+        </li>
+        <li>
+          Helps therapists to explain treatment models in more effective way
+        </li>
+        <li>Avoids misunderstanding between patients & therapist</li>
       </ul>
+      <h2 className="text-3xl font-semibold text-orange-400 mt-8 my-4">
+        Development
+      </h2>
+      <p className="leading-relaxed ">
+        This application is still in development process. Even though it has
+        some of the fundamental functionalities, there are features in being
+        developed as follows:
+        <ul className="list-disc list-inside  ml-6">
+          <li>
+            Currently only YouTube links can be added, it will be updated to
+            upload with jpeg/png format and mp4 video format.
+          </li>
+          <li>
+            Therapists can only add their profile picture via link. It will be
+            updated to upload with jpeg/png format
+          </li>
+          <li>Treatment models will be more detailed</li>
+          <li>
+            Rating is currently working, however the star chart will be dynamic.
+          </li>
+          <li>User Interface will be updated, styling will be improved</li>
+          <li>
+            Currently is not mobile friendly, once the features are all set, it
+            will be implemented
+          </li>
+        </ul>
+      </p>
+      <h2 className="text-3xl font-semibold text-orange-400 mt-8 my-4">
+        Demo Instructions
+      </h2>
+      <p className="leading-relaxed ">Use this way...</p>
+      <h2 className="text-3xl font-semibold text-orange-400 mt-8 my-4">
+        Suggestions/Feedback
+      </h2>
+      <p className="leading-relaxed ">Give me feedback</p>
     </section>
   );
 };
