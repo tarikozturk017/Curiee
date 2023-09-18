6707c37f6a6001f5efea6bf4d1743172dab8cec7
tarikozturk017 <tarik.ozturkk1@gmail.com>
2023-09-18 18:29:03 -0400

    PUT cors update

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: ["https://curiee-app.vercel.app", "http://localhost:3000"], // to be changed to real one
-    methods: ["POST", "GET"],
+    methods: ["POST", "GET", "PUT"],
     credentials: true,
   })
 ); // avoid cross origin errors
