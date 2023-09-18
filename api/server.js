549fb72980956dfc90f5b07505c2320e932253da
tarikozturk017 <tarik.ozturkk1@gmail.com>
2023-09-18 10:49:09 -0400

    backend vercel config

--- api/server.js
+++ api/server.js
@@ -1,32 +1,38 @@
-require('dotenv').config()
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 // const authRoutes = require('./routes/auth');
 
-const exercisesRouter = require('./routes/exercises');
-const patientsRouter = require('./routes/patients');
-const therapistsRouter = require('./routes/therapists');
+const exercisesRouter = require("./routes/exercises");
+const patientsRouter = require("./routes/patients");
+const therapistsRouter = require("./routes/therapists");
 
 const app = express();
 
 app.use(express.json());
-app.use(cors()); // avoid cross origin errors
+app.use(
+  cors({
+    origin: ["https://deploy-mern-1wh1.vercel.app"], // to be changed to real one
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+); // avoid cross origin errors
 
-const uri = process.env.API_URI
+const uri = process.env.API_URI;
 
-mongoose.connect(uri, {
+mongoose
+  .connect(uri, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(
-    () => console.log("Connected to DB")
-    ).catch(console.error)
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to DB"))
+  .catch(console.error);
 
+// app.use('/api/auth', authRoutes);
 
-// app.use('/api/auth', authRoutes);  
+app.use("/patient", patientsRouter);
+app.use("/exercise", exercisesRouter);
+app.use("/therapist", therapistsRouter);
 
-app.use('/patient', patientsRouter);
-app.use('/exercise', exercisesRouter);
-app.use('/therapist', therapistsRouter);
-
-app.listen(3001, () => console.log("Server started on port 3001"))
+app.listen(3001, () => console.log("Server started on port 3001"));
